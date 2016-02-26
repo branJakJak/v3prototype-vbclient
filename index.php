ec2b74d5fcd5beb8f786637d5bfccba3307eb8e5
Your Name <you@example.com>
2016-02-27 03:21:15 +0800

    ds

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 <?php
 
-
-defined('YII_DEBUG') or define('YII_DEBUG',true);
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
-
-
-// change the following paths if necessary
-$yii=dirname(__FILE__).'/../yii/framework/yii.php';
+$yii=dirname(__FILE__).'/protected/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 
